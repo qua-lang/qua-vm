779d5ed3e88fdf50ce5e62f99a98da6b6ff7173d
Manuel Simoni <msimoni@gmail.com>
2018-04-24 13:35:28 +0200

    Remove superfluous assert impl

--- js/test.js
+++ js/test.js
@@ -1,7 +1,6 @@
 // Adds utility functions for testing the built-ins to a VM
 var deep_equal = require("deep-equal");
 module.exports = function(vm, e) {
-    function assert(x) { if (!x) return vm.error("assertion failure"); }
-    vm.defun(e, vm.sym("%%assert"), vm.jswrap(assert));
+    vm.defun(e, vm.sym("%%assert"), vm.jswrap(vm.assert));
     vm.defun(e, vm.sym("%%deep-equal"), vm.jswrap(deep_equal));
 };
