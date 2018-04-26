8a139ef1da13c135496b6853b12989f1ab2776e8
Manuel Simoni <msimoni@gmail.com>
2018-04-26 14:45:07 +0200

    Add collection.lisp

--- build/compile-init.js
+++ build/compile-init.js
@@ -1,3 +1,10 @@
 var fs = require("fs");
 var parser = require("../js/read");
-console.log("module.exports.main = " + JSON.stringify(parser.parse_sexp(fs.readFileSync("lisp/init.lisp", "utf8"))));
+var paths = ["lisp/init.lisp", "lisp/collection.lisp"];
+var code = paths.map(file).join("\n");
+
+function file(path) {
+    return fs.readFileSync(path, "utf8")
+}
+
+console.log("module.exports.main = " + JSON.stringify(parser.parse_sexp(code)));
