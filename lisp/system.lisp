5f41c9dc621b6fe361578a33ab353bf7d1accaa3
Manuel Simoni <msimoni@gmail.com>
2018-05-07 18:39:03 +0200

    Use FOR-EACH in LOAD-SYSTEM

--- lisp/system.lisp
+++ lisp/system.lisp
@@ -4,10 +4,10 @@
 
 (def #'load-system
   (wrap (vau (path . opt-env) denv
-             (let* ((env (optional opt-env denv))
-                    (#'defsystem
-                     (vau (name :components components) #ign
-                       (list-for-each (lambda (component)
-                                        (load (node:join-paths (node:dirname path) component) env))
-                                      components))))
-               (load path)))))
+          (let* ((env (optional opt-env denv))
+                 (#'defsystem
+                  (vau (name :components components) #ign
+                    (for-each (lambda (component)
+                                (load (node:join-paths (node:dirname path) component) env))
+                              components))))
+            (load path)))))
