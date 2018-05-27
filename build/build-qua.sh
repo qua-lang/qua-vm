0e154f9b9e79d8e4937e14750cae15c4abe5287f
Manuel Simoni <msimoni@gmail.com>
2018-05-27 18:31:33 +0200

    Build browser-specific out file

--- build/build-qua.sh
+++ build/build-qua.sh
@@ -1,3 +1,4 @@
 node build/compile-init.js > build/out/init.js
+node build/compile-init-browser.js > build/out/init-browser.js
 node build/compile-test.js > build/out/test.js
 browserify js/main.js --standalone qua > build/out/qua.js
---
+++ build/compile-init-browser.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+var parser = require("../js/read");
+var paths = ["lisp/bootstrap.lisp"];
+var code = paths.map(file).join("\n");
+
+function file(path) {
+    return fs.readFileSync(path, "utf8")
+}
+
+console.log("module.exports.main = " + JSON.stringify(parser.parse_sexp(code)));
