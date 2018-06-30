ea8e040f1660bd73da28d32942a0eaa191f65659
Manuel Simoni <msimoni@gmail.com>
2018-06-30 19:46:55 +0200

    Don't use Browserify standalone mode to get window.qua global variable, because this doesn't seem to work if we use expose to create the qua-user-code package.  Instead simply re-export js/main.js as global.qua

--- js/arch-browser.js
+++ js/arch-browser.js
@@ -2,5 +2,6 @@
 // mapping so that this gets used instead of arch.js if we're doing a
 // browser build.
 module.exports = function(vm, root_env) {
+    global.qua = require("./main.js");
     // Here we could do browser-specific exports.
 };
