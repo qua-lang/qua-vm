78cb185cf1ce9564f223cd6d4978993343530d38
Manuel Simoni <msimoni@gmail.com>
2018-07-22 15:39:22 +0200

    Use DEFUN/ENV

--- lisp/arch.lisp
+++ lisp/arch.lisp
@@ -29,21 +29,19 @@
   (list* #'progn (%%parse-bytecode (%%parse-sexp (read-file-as-string path)))))
 
 ;; Loads a file into the current or specified environment.
-(def #'load
-  (wrap (vau (path . opt-env) denv
-          (user-eval (read-file path) (optional opt-env denv)))))
+(defun/env load (path . opt-env) denv
+  (user-eval (read-file path) (optional opt-env denv)))
 
 ;; Loads all components of a system into the current or specified
 ;; environment.
-(def #'load-system
-  (wrap (vau (path . opt-env) denv
-          (let* ((env (optional opt-env denv))
-                 (#'defsystem
+(defun/env load-system (path . opt-env) denv
+  (let* ((env (optional opt-env denv))
+         (#'defsystem
                   (vau (name :depends-on deps :components components) #ign
-                    (list-for-each (lambda (dep)
-                                     (load-system (node:join-paths (node:dirname path) dep) env))
-                                   deps)
-                    (list-for-each (lambda (component)
-                                     (load (node:join-paths (node:dirname path) component) env))
-                                   components))))
-            (load path)))))
+                       (list-for-each (lambda (dep)
+                                        (load-system (node:join-paths (node:dirname path) dep) env))
+                                      deps)
+                       (list-for-each (lambda (component)
+                                        (load (node:join-paths (node:dirname path) component) env))
+                                      components))))
+    (load path)))
