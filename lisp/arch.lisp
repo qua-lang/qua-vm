1e55db3dda485d9254b0aecb1f83b6d819b37551
Manuel Simoni <msimoni@gmail.com>
2018-07-23 14:55:43 +0200

    Reformat arch.lisp

--- lisp/arch.lisp
+++ lisp/arch.lisp
@@ -39,11 +39,11 @@
 (defun/env load-system (path . opt-env) denv
   (let* ((env (optional opt-env denv))
          (#'defsystem
-                  (vau (name :depends-on deps :components components) #ign
-                       (list-for-each (lambda (dep)
-                                        (load-system (node:join-paths (node:dirname path) dep) env))
-                                      deps)
-                       (list-for-each (lambda (component)
-                                        (load (node:join-paths (node:dirname path) component) env))
-                                      components))))
+           (vau (name :depends-on deps :components components) #ign
+                (list-for-each (lambda (dep)
+                                 (load-system (node:join-paths (node:dirname path) dep) env))
+                               deps)
+                (list-for-each (lambda (component)
+                                 (load (node:join-paths (node:dirname path) component) env))
+                               components))))
     (load path)))
