ecc46e14d2521bc551cd704ec9dbd2409ee27a64
Manuel Simoni <msimoni@gmail.com>
2018-08-01 15:04:16 +0200

    Push userspace in qua, use __dirname to access repl.lisp

--- cmd/qua.js
+++ cmd/qua.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 // This is the main entry point for the `qua' UNIX command.
-console.log(require("../src/main.js").vm().eval_string("(load \"lisp/repl.lisp\")"));
+console.log(require("../src/main.js").vm().eval_string("(push-userspace (load \"" + __dirname + "/../lisp/repl.lisp\"))"));
