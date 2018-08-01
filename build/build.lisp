a9dab2781107f32dd99d3e8b21b0ae67911d9638
Manuel Simoni <msimoni@gmail.com>
2018-08-01 15:02:42 +0200

    BUILD

--- build/build.lisp
+++ build/build.lisp
@@ -6,22 +6,9 @@
 (def bytefile (node:require "../build/bytefile"))
 (def #'write-bytecode-file (.write_bytecode_file bytefile))
 
-(write-bytecode-file (js-array "lisp/bootstrap.lisp" "lisp/arch-browser.lisp")
-		     "build/out/bootstrap-browser.json")
-
-(write-bytecode-file (js-array)
-		     "build/out/prod-browser.json")
-
-(write-bytecode-file (js-array "lisp/repl.lisp")
-		     "build/out/repl-browser.json")
-
-(write-bytecode-file (js-array "test/lisp/test.lisp" "test/lisp/test-browser.lisp")
-		     "build/out/test-browser.json")
-
-;;;; Build browser Browserify "images"
+;;;; Build browser Browserify bundles
 
 (def #'browserify (node:require "browserify"))
-
 (defun build-browser-image (bytefile outfile test?)
   (let ((b (browserify "./build/browser-template")))
     (@require b bytefile (js-object :expose "qua-user-bytecode"))
@@ -29,14 +16,27 @@
       (@require b "deep-equal"))
     (@pipe (@bundle b) (node:create-write-stream outfile))))
 
-(build-browser-image "./build/out/prod-browser.json"
-		     "build/out/qua.js"
-		     #f)
+(defun build ()
+  (write-bytecode-file (js-array "lisp/bootstrap.lisp" "lisp/arch-browser.lisp")
+		       "build/out/bootstrap-browser.json")
+  
+  (write-bytecode-file (js-array)
+		       "build/out/prod-browser.json")
+  
+  (write-bytecode-file (js-array "lisp/repl.lisp")
+		       "build/out/repl-browser.json")
+  
+  (write-bytecode-file (js-array "test/lisp/test.lisp" "test/lisp/test-browser.lisp")
+		       "build/out/test-browser.json")
 
-(build-browser-image "./build/out/repl-browser.json"
-		     "build/out/qua-repl.js"
-		     #f)
-
-(build-browser-image "./build/out/test-browser.json"
-		     "build/out/qua-test.js"
-		     #t)
+  (build-browser-image "./build/out/prod-browser.json"
+		       "build/out/qua.js"
+		       #f)
+  
+  (build-browser-image "./build/out/repl-browser.json"
+		       "build/out/qua-repl.js"
+		       #f)
+  
+  (build-browser-image "./build/out/test-browser.json"
+		       "build/out/qua-test.js"
+		       #t))
