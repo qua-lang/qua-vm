6dad23385a04dda7232d04ad232d1fd7bb38c0c4
Manuel Simoni <msimoni@gmail.com>
2018-08-01 15:03:21 +0200

    Document qua-run

--- cmd/qua-run.js
+++ cmd/qua-run.js
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
+// Runs a Qua file and quits.
 // HACKCKCKKCKCKCK -- why is this so horrible?
-require("../src/main.js").vm().eval_string("(load \"" + process.argv[2] + "\")");
+console.log(require("../src/main.js").vm().eval_string("(load \"" + process.argv[2] + "\")"));
